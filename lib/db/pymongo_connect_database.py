355869144d860ccdd7c9912f6ed2ef9876272651
koreanthuglife <rlghks358@naver.com>
2023-06-15 14:00:54 +0900

    .env 파일 추가

--- lib/db/pymongo_connect_database.py
+++ lib/db/pymongo_connect_database.py
@@ -1,10 +1,20 @@
 import certifi
+from pydantic import BaseSettings
 from pymongo import MongoClient
 
 
+class Settings(BaseSettings):
+    DB: str = 'dev'
+
+    class Config:
+        env_file = '.env'
+
+
 def connect_database():
+    settings = Settings()
     # 드라이버를 통해 연결할 데이터베이스 URL
-    DATABASE_URL = "mongodb+srv://rlghks3004:2Kf6alnDRXkc2pM9@algorithm.4hwfeom.mongodb.net/?retryWrites=true&w=majority"
+
+    DATABASE_URL = settings.DB
 
     ca = certifi.where()
 
