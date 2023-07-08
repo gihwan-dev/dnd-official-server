6dc9f2731d6faca9c0a6b9bfbd8ede07c243bbf6
최기환 <rlghks9806@gmail.com>
2023-07-08 16:43:44 +0900

    로그인 API 구현 완료

--- app/main.py
+++ app/main.py
@@ -4,8 +4,24 @@ from fastapi import FastAPI
 from app.order.order import router as order_router
 from app.user.user import router as user_router
 from app.store.store import router as store_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
+
+origins = [
+  "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def greeting():
